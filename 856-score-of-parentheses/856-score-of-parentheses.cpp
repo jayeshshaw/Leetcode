d25f37fe830e775227433c950162cce91a4df62f
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-03-17 13:49:22 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (45.05%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        int ans=0,open=0;
+        for(int i=0;i<s.size();i++){
+            
+            if(s[i]=='(')open++;
+            
+            else{
+                open--;
+                if(s[i-1]=='(')ans+=pow(2,open);
+            }
+        }
+        return ans;
+    }
+};
