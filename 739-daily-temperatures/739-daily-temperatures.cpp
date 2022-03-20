72c242efcab3cc61e66e1ca8c431ac36aac41fe4
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-03-20 20:12:22 +0530

    Time: 217 ms (58.14%), Space: 88.9 MB (39.93%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        vector<int> ans(t.size(),0);
+        stack<int> s;
+        for(int i=0;i<t.size();i++){
+           while(!s.empty() && t[i]>t[s.top()]){
+               ans[s.top()]=i-s.top();
+               s.pop();
+           }
+            s.push(i);
+        }
+        // while(!s.empty()){
+        //     ans[s.top()]=0;
+        //     s.pop();
+        // }
+        return ans;
+    }
+};
