e0e0c880c4a9c72ed749de941e69cd925abb08ee
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-03-20 20:18:49 +0530

    Time: 4 ms (42.59%), Space: 6.5 MB (88.76%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0;
+        int i=s.size()-1;
+        if(s[i]==' ') {{while(i>=0 && s[i]==' ')i--;}}
+        // cout<<s[i]<<".";
+        while(i>=0 && s[i]!=' '){ans++;i--;}
+        return ans;
+    }
+};
