42ece71704a37eb8265fe085794880ca81b835d9
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-06-23 13:26:51 +0530

    Time: 69 ms (31.58%), Space: 30.1 MB (26.51%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int f(int i, int j, string &s, string &t,vector<vector<int>> &dp){
+    int f(int i, int j, string s, string t,vector<vector<int>> &dp){
         if(j<0)return 1;
         if(i<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
@@ -11,20 +11,21 @@ public:
     }
     
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size()+1,vector<int> (t.size()+1,-1));
+        vector<vector<double>> dp(s.size()+1,vector<double> (t.size()+1,0));
         
-        return f(s.size()-1,t.size()-1,s,t,dp);
-//         for(int i=0;i<s.size();i++)dp[i][0]=1;
-//         for(int j=0;j<t.size();j++)dp[0][j]=0;
+        // return f(s.size()-1,t.size()-1,s,t,dp);
         
-//         for(int i=1;i<s.size()+1;i++){
-//             for(int j=1;j<t.size()+1;j++){
-//                 if(s[i-1]==t[j-1]) 
-//                     dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
-//                 else 
-//                     dp[i][j]=dp[i-1][j];
-//             }
-//         }
-//         return dp[s.size()][t.size()];
+        //TABULATION
+        for(int i=0;i<=s.size();i++)dp[i][0]=1;
+        
+        for(int i=1;i<s.size()+1;i++){
+            for(int j=1;j<t.size()+1;j++){
+                if(s[i-1]==t[j-1]) 
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+                else 
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[s.size()][t.size()];
     }
 };
