a40cd8cafaef0e030d03a443239aa303b889056e
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-06-23 19:42:50 +0530

    Time: 8 ms (48.83%), Space: 12 MB (21.97%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int f(int i, int b, int k, vector<int> &v,vector<vector<vector<int>>> &dp){
-        if(i>=v.size() || k==0)return 0;
+    int f(int i, int b, vector<int> &v,vector<vector<int>> &dp){
+        if(i>=v.size())return 0;
         
-        if(dp[i][b][k]!=-1)return dp[i][b][k];
+        if(dp[i][b]!=-1)return dp[i][b];
         
         if(b==1)
-            return  dp[i][b][k]= max (-v[i]+f(i+1,0,k,v,dp) , f(i+1,1,k,v,dp));
+            return  dp[i][b]= max (-v[i]+f(i+1,0,v,dp) , f(i+1,1,v,dp));
         else 
-            return  dp[i][b][k]= max (v[i]+f(i+2,1,k-1,v,dp) , f(i+1,0,k,v,dp));
+            return  dp[i][b]= max (v[i]+f(i+2,1,v,dp) , f(i+1,0,v,dp));
     }
     
     int maxProfit(vector<int>& v) {
-        int k=v.size()/3+1; 
-        vector<vector<vector<int>>> dp(v.size()+1, vector<vector<int>> (2, vector<int> (k+1,-1)));
-        return f(0,1,k,v,dp);
+        vector<vector<int>> dp(v.size()+1, vector<int> (2,-1));
+        return f(0,1,v,dp);
     }
 };
