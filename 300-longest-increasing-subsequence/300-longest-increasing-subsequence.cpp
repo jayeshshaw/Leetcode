d8ee866e31a3f34a41d392d2150c36fdc52a7873
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-06-24 21:23:55 +0530

    Time: 732 ms (13.19%), Space: 10.6 MB (22.09%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -14,18 +14,20 @@ public:
     }
     
     int lengthOfLIS(vector<int>& v) {
-        vector<vector<long>> dp(v.size()+1,vector<long> (v.size()+1,0));
+        // vector<vector<long>> dp(v.size()+1,vector<long> (v.size()+1,0));
+        vector<int> next(v.size()+1,0),cur(v.size()+1,0);
         
         for(int i=v.size()-1;i>=0;i--){
             for(int pre=i-1;pre>=-1;pre--){
                 int a=0,b=0;
-                if(pre==-1 || v[i]> v[pre])a= 1 + dp[i+1][i+1];
-                b=dp[i+1][pre+1];
+                if(pre==-1 || v[i]> v[pre])a= 1 + next[i+1];
+                b=next[pre+1];
 
-                dp[i][pre+1]= max(a,b);
+                cur[pre+1]= max(a,b);
             }
+            next=cur;
         }
-        return dp[0][0];
+        return cur[0];
         
         // return f(0,-1,v,dp);
     }
