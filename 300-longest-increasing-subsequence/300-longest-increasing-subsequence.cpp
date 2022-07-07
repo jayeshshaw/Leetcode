f1156b4fdf1bf76bace8d3d943011af0f7357139
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-07-07 13:58:42 +0530

    Time: 1839 ms (5.00%), Space: 197.7 MB (11.31%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& v) {
-        vector<int> help;
-        help.push_back(v[0]);
-        for(int i=1;i<v.size();i++){
-            if(v[i]>help[help.size()-1])help.push_back(v[i]);
-            else{
-                int ind=lower_bound(help.begin(),help.end(),v[i])-help.begin();
-                help[ind]=v[i];
-            }
-            
+    
+    int f(int i, int j, vector<int>& s, vector<int>& p,vector<vector<int>> &dp){
+        if(i<0 || j<0)return 0;
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==p[j])return dp[i][j]= 1+f(i-1,j-1,s,p,dp);
+        
+        return  dp[i][j]=  max(f(i-1,j,s,p,dp),f(i,j-1,s,p,dp));
+    }
+    
+    int lengthOfLIS(vector<int>& s) {
+        vector<int> p=s;
+        sort(p.begin(),p.end());
+        for(int i=1;i<p.size();i++){
+            while(i>0 && i<p.size() && p[i]==p[i-1])p.erase(p.begin()+i);
         }
-        return help.size();
+        vector<vector<int>> dp(s.size()+1,vector<int> (p.size()+1,-1));
+        return f(s.size()-1, p.size()-1,s,p,dp);
     }
 };
