ceeef700c0dc596f62a9ae9d2c3661a0418eb2f6
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-07-07 12:51:42 +0530

    Time: 7 ms (57.29%), Space: 7.2 MB (48.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool f(string &a, string &b, string &c, int i, int j, int k,vector<vector<vector<int>>> &dp){
-        if(k==c.size())return true;
+    bool f(string &a, string &b, string &c, int i, int j,vector<vector<int>> &dp){
+        if(i+j==c.size())return true;
         
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
+        if(dp[i][j]!=-1)return dp[i][j];
         
         bool picka=false,pickb=false;
-        if(i<a.size() && a[i]==c[k]){picka=f(a,b,c,i+1,j,k+1,dp);}
-        if(j<b.size() && b[j]==c[k]){pickb=f(a,b,c,i,j+1,k+1,dp);}
+        if(i<a.size() && a[i]==c[i+j]){picka=f(a,b,c,i+1,j,dp);}
+        if(j<b.size() && b[j]==c[i+j]){pickb=f(a,b,c,i,j+1,dp);}
         
-        return dp[i][j][k]= picka or pickb;
+        return dp[i][j]= picka or pickb;
     }
     
     
     bool isInterleave(string s1, string s2, string s3) {
         int i=0,j=0,k=0;
-        vector<vector<vector<int>>> dp(s1.size()+1,vector<vector<int>> (s2.size()+1, vector<int> (s3.size()+1,-1)));
+        vector<vector<int>> dp(s1.size()+1,vector<int> (s2.size()+1,-1));
         if(s3.size()!=s1.size()+s2.size())return 0;
-        return f(s1,s2,s3,i,j,k,dp);
+        return f(s1,s2,s3,i,j,dp);
     }
 };
