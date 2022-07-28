0bf8ab986d0e937262916eb058dfb3086458e3b2
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-07-28 12:42:26 +0530

    Time: 32 ms (5.61%), Space: 7.3 MB (77.97%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> m;
+        for(auto i: s)m[i]++;
+        for(auto i: t)m[i]--;
+        for(auto i: m){
+            if(i.second!=0)return false;
+        }
+        return true;
+    }
+};
