5fdb8753bb57ad1227927f9d732e4b591dc23718
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-08-11 22:31:22 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.67%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,18 +4,11 @@ public:
         int m=mat.size();
         for(int i=0;i<m;i++){
             for(int j=0;j<i;j++){
-                int temp=mat[i][j];
-                mat[i][j]=mat[j][i];
-                mat[j][i]=temp;
+                swap(mat[i][j],mat[j][i]);
             }
         }
-        int n=mat[0].size();
         for(int i=0;i<m;i++){
-            for(int j=0;j<n/2;j++){
-                int t=mat[i][j];
-                mat[i][j]=mat[i][n-j-1];
-                mat[i][n-j-1]=t;
-            }
+            reverse(begin(mat[i]),end(mat[i]));
         }
         
     }
