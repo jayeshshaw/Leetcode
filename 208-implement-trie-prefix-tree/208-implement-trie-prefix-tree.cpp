74224950cf9626101205f60c31ae9d971b8ead0c
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-09-28 15:14:28 +0530

    Time: 213 ms (10.51%), Space: 36.3 MB (93.29%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,54 +1,25 @@
-struct Node{
-    Node * links[26];
-    bool flag;
-    
-    bool contains(char c){
-        return links[c-'a']!=NULL;
-    }
-    
-    void put(char c,Node* node){
-        links[c-'a']=node;
-    }
-};
-
 class Trie {
-private: Node* root=NULL;
 public:
+    unordered_map<string,int> s,sw;
     Trie() {
-        root=new Node();
+        
     }
     
     void insert(string word) {
-        Node* node=root;
-        for(int i=0;i<word.size();i++){
-            if(!node->contains(word[i])){
-                node->put(word[i],new Node());
-            }
-            node=node->links[word[i]-'a'];
+        s[word]=1;
+        string p="";
+        for(auto i: word){
+            p+=i;
+            sw[p]=1;
         }
-        node->flag=true;
     }
     
     bool search(string word) {
-        Node* node=root;
-        for(int i=0;i<word.size();i++){
-            if(!node->contains(word[i])){
-                return false;
-            }
-            node=node->links[word[i]-'a'];
-        }
-        return node->flag==true;
+        return s[word]!=0;
     }
     
-    bool startsWith(string word) {
-        Node* node=root;
-        for(int i=0;i<word.size();i++){
-            if(!node->contains(word[i])){
-                return false;
-            }
-            node=node->links[word[i]-'a'];
-        }
-        return true;
+    bool startsWith(string prefix) {
+        return sw[prefix]!=0;
     }
 };
 
