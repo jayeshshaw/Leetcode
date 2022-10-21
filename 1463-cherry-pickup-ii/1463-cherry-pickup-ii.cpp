6aa5c65f494d790c248b4c04aaad0a79dfc38298
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-10-21 22:57:28 +0530

    Time: 211 ms (55.13%), Space: 17 MB (48.93%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
     
-    int f(int i,int j1,int j2, int n,int m, vector<vector<int>>& v,vector<vector<vector<int>>> &dp){
-        // if(j1>=m || j1<0 || j2>=m || j2<0)return -1e8;
-        if(i==n-1){
-            if(j1==j2)return v[i][j1];
-            return v[i][j1]+v[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
-        int mx=-1e8;
-        for(int p=-1;p<2;p++){
-            for(int q=-1;q<2;q++){
-                int newj1=j1+p,newj2=j2+q;
-                if(newj1<0 || newj1>=m || newj2<0 || newj2>=m)continue;
+//     int f(int i,int j1,int j2, int n,int m, vector<vector<int>>& v,vector<vector<vector<int>>> &dp){
+//         // if(j1>=m || j1<0 || j2>=m || j2<0)return -1e8;
+//         if(i==n-1){
+//             if(j1==j2)return v[i][j1];
+//             return v[i][j1]+v[i][j2];
+//         }
+//         if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
+//         int mx=-1e8;
+//         for(int p=-1;p<2;p++){
+//             for(int q=-1;q<2;q++){
+//                 int newj1=j1+p,newj2=j2+q;
+//                 if(newj1<0 || newj1>=m || newj2<0 || newj2>=m)continue;
                 
-                int ans=0;
-                if(newj1==newj2)ans+=v[i][j2];
-                else ans+=v[i][j1]+v[i][j2];
-                ans+=f(i+1,newj1,newj2,n,m,v,dp);
-                mx=max(mx,ans);
+//                 int ans=0;
+//                 if(newj1==newj2)ans+=v[i][j2];
+//                 else ans+=v[i][j1]+v[i][j2];
+//                 ans+=f(i+1,newj1,newj2,n,m,v,dp);
+//                 mx=max(mx,ans);
+//             }
+//         }
+//         return dp[i][j1][j2]= mx;
+//     }
+    
+    int f(int i,int j1,int j2,int &n, int &m,  vector<vector<int>>& v,vector<vector<vector<int>>> &dp){
+        if(i==n)return 0;
+        
+        if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
+        int ans=0;
+        ans+=v[i][j1];
+        if(j1!=j2)ans+=v[i][j2];
+        int res=ans;
+        for(int a=-1;a<2;a++){
+            for(int b=-1;b<2;b++){
+                int newj1=j1+a,newj2=j2+b;
+                if(newj1<0 || newj1>=m || newj2<0 || newj2>=m)continue;
+                int abhi=ans;
+                abhi+=f(i+1,newj1,newj2,n,m,v,dp);
+                res=max(res,abhi);
             }
         }
-        return dp[i][j1][j2]= mx;
+        return dp[i][j1][j2]=res;
     }
    
     
