a591f9b2ec8a73748e8d56e08ed7dc2bccde9a06
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2022-12-24 13:41:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.49%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    int mod=1000000007;
-    vector<vector<int>> dp;
-    long long f(int i,int khali, int n){
-        if(i>n)return 0;
-        if(i==n)return khali==0;
-        
-        if(dp[i][khali]!=-1)return dp[i][khali];
-        
-        long long ans=0;
-        if(khali==0){
-            ans+=f(i+1,0,n)%mod;
-            ans+=f(i+2,0,n)%mod;
-            ans+=2*f(i+2,1,n)%mod;
-        }
-        else{
-            ans+=f(i+1,0,n)%mod;
-            ans+=f(i+1,1,n)%mod;
-        }
-        return dp[i][khali]=ans%mod;
-    }
-    
-    
+    int mod=1000000007;    
     int numTilings(int n) {
-        dp.resize(n+1,vector<int> (2,-1));
-        return f(0,0,n);
+        if(n<=2)return n;
+        if(n==3)return 5;
+        long long a=1,b=2,c=5;
+        for(int i=4;i<=n;i++){
+            long long abhi=(2*c)%mod+a;
+            a=b,b=c,c=abhi%mod;
+        }
+        return c;
     }
 };
