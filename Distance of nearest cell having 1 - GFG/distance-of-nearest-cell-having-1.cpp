7fad98c24d007b5017777482f8ef2a302709d539
Jayesh Kumar Shaw <75195156+jayeshshaw@users.noreply.github.com>
2023-01-07 00:59:49 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -3,66 +3,46 @@
 using namespace std;
 
 // } Driver Code Ends
-
-class Solution
+class Solution 
 {
     public:
-	vector<int>dx = {1,-1,0,0};
-	vector<int>dy = {0,0,1,-1};
-	
-	//Function to check whether the cell is within the matrix bounds.
-	bool isValid(int x, int y, int n, int m){
-		return (x >=0 and x < n and y >=0 and y < m);
-	}
-	
-	//Function to find distance of nearest 1 in the grid for each cell.
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
-		int n = grid.size();
-		int m = grid[0].size();
-		
-		//queue to store the cell indexes which have grid value 1.
-		queue<pair<int, int>>q;
-		
-		//using dp list which will store the output.
-		vector<vector<int>>dp(n, vector<int>(m, INT_MAX));
-		
-		//traversing all the cells of the matrix.
-		for(int i = 0; i < n; i++)
-		{
-			for(int j = 0; j < m; j++)
-			{
-			    //if grid value is 1, we update the dp value at same cell as 0 
-			    //and push the cell indexes into queue.
-				if(grid[i][j] == 1){
-					dp[i][j] = 0;
-					q.push({i,j});
-				}
-			}
-		}
-		
-		while(!q.empty())
-		{
-		    //storing the cell indexes at top of queue and popping them.
-			int x = q.front().first;
-			int y = q.front().second;
-			q.pop();
-			
-			//iterating over the adjacent cells.
-			for(int i = 0; i < 4; i++)
-			{
-				int n_x = x + dx[i];
-				int n_y = y + dy[i];
-				if(isValid(n_x, n_y, n, m) and dp[n_x][n_y] > dp[x][y] + 1)
-				{
-				    //updating dp and pushing cell indexes in queue.
-					dp[n_x][n_y] = dp[x][y] + 1;
-					q.push({n_x, n_y});
-				}
-			}
-		}
-		//returning the dp list.
-		return dp;
+	    int n=grid.size(),m=grid[0].size();
+	    vector<vector<int>> vis(n, vector<int> (m,0));
+	    vector<vector<int>> dist(n, vector<int> (m,0));
+	    
+	    queue<vector<int>> q;
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(grid[i][j]==1){
+	                q.push({i,j,0});
+	                vis[i][j]=1;
+	            }
+	        }
+	    }
+	    
+	    int delrow[]={-1,0,1,0};
+	    int delcol[]={0,1,0,-1};
+	    
+	    while(q.size()>0){
+	        auto v=q.front();q.pop();
+	        int row=v[0],col=v[1],steps=v[2];
+	        dist[row][col]=steps;
+	        
+	        for(int i=0;i<4;i++){
+	            int nrow=row+delrow[i];
+	            int ncol=col+delcol[i];
+	            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+	                q.push({nrow,ncol,steps+1});
+	                vis[nrow][ncol]=1;
+	            }
+	        }
+	        
+	    }
+	    return dist;
+	    
 	}
 };
 
